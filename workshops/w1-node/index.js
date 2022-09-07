af37ae3c6004fcad601c0f868af8b0d2de639935
Sharon Ku <71299609+sharon-ku@users.noreply.github.com>
2022-09-06 21:32:25 -0400

    w1: set up server routing --- now "Hello world" is displayed!

--- workshops/w1-node/index.js
+++ workshops/w1-node/index.js
@@ -9,12 +9,25 @@ const portNumber = 4200;
 // Create an instance of the Express object (to access the methods and properties available)
 const app = express();
 
+// Define the app's routing -- HTTP server gives a router by default
+app.get('/', requestHandler);
+
+// The request is the URL that the client requested
+// The response is sent back to the client
+function requestHandler(request,response){
+    // send a default response to the client...
+    response.send("HELLO WORLD");
+        console.log(request); //built in
+        console.log(response); //built  in
+    console.log(request.url);
+}
+
+
 // Allow the server to listen for incoming requests on port 4200
 // Once this starts running, it never stops until you quit it
 app.listen(portNumber, function() {
     console.log("Server is running on port " + portNumber);
 });
-
 
 
 
