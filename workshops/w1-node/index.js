8c1c112bbcbb9a253135d3debd5f4b93edac20e9
Sharon Ku <71299609+sharon-ku@users.noreply.github.com>
2022-09-06 21:45:35 -0400

    w1: set up fruitRoute and vegRoute

--- workshops/w1-node/index.js
+++ workshops/w1-node/index.js
@@ -10,18 +10,40 @@ const portNumber = 4200;
 const app = express();
 
 // Define the app's routing -- HTTP server gives a router by default
+// app.get is a specific case of app.use
 app.get('/', requestHandler);
+// Forward the requests.... (using a filter)
+// vegRoutes and fruitRoutes are my middleware: they are scripts that run
+app.use('/veg', vegRoutes);
+app.use('/fruit',fruitRoutes);
 
 // The request is the URL that the client requested
 // The response is sent back to the client
 function requestHandler(request,response){
     // send a default response to the client...
     response.send("HELLO WORLD");
-        console.log(request); //built in
-        console.log(response); //built  in
+    console.log(request); //built in
+    console.log(response); //built  in
     console.log(request.url);
 }
 
+function fruitRoutes(req, res, next){
+    // req is the Node.js http request object
+    // res is the Node.js http response object
+    // next is a function to call to invoke the next middleware
+     console.log("IN FRUIT FUNCTION ");
+     console.log(req);
+     res.send("WE ARE HERE FRUIT ROUTE");
+}
+
+function vegRoutes(req, res, next){
+    // req is the Node.js http request object
+   // res is the Node.js http response object
+   // next is a function to call to invoke the next middleware
+   console.log("IN VEG FUNCTION ");
+   console.log(req);
+  res.send("WE ARE HERE VEG ROUTE");
+ }
 
 // Allow the server to listen for incoming requests on port 4200
 // Once this starts running, it never stops until you quit it
