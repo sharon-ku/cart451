69580747195153f7c1f237603444a0d765ffea9f
Sharon Ku <71299609+sharon-ku@users.noreply.github.com>
2022-09-13 19:33:57 -0400

    w2: save a file in client folder

--- workshops/w1-node/index.js
+++ workshops/w1-node/index.js
@@ -1,5 +1,6 @@
 // Import the library
-let express = require('express');
+const express = require('express');
+const fileuploadMiddleWare = require("express-fileupload");
 
 // Define the port number where the server should listen for requests
 // We can have multiple web servers running on a machine
@@ -27,6 +28,9 @@ app.use('/fruit', fruitRoutes);
 // When I type localhost:4200/banana, it will take me to banana.html
 app.use('/banana', bananaRoute);
 app.use('/passingTheVars', handleGetVars);
+
+app.use(fileuploadMiddleWare());
+app.use('/dataUpload',handlePostedData);
 
 // The request is the URL that the client requested
 // The response is sent back to the client
@@ -74,6 +78,33 @@ function handleGetVars(request, response, next) {
     console.log(request.query);
     response.send("GOT IT! THANKS!");
 }
+
+function handlePostedData(request,response){
+    console.log(request.body); //body of packet
+    console.log(request.files); //request
+
+    if(!request.files)
+    {
+        response.send("File was not found");
+        return;
+    }
+    // using the name attributes of the form fields ...
+    console.log("the color chosen:: "+request.body.color);
+      console.log("the favorite city chosen:: "+request.body.city);
+ 
+    // here is the field name of the form
+    let  temp_file = request.files.imageF;
+ 
+     let imagePath  = __dirname + '/public/images/'+request.files.imageF.name;
+    // Use the mv() method to place the file somewhere on your server
+    temp_file.mv(imagePath, function(err) {
+    if (err)
+        return response.status(500).send(err);
+        response.send('File uploaded!');
+     });
+ }
+
+
 
 // Allow the server to listen for incoming requests on port 4200
 // Once this starts running, it never stops until you quit it
