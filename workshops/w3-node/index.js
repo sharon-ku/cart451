a3033cff9c6591f9f4aae02de396d29602d9893e
Sharon Ku <71299609+sharon-ku@users.noreply.github.com>
2022-09-20 19:59:39 -0400

    w3 issue with not using classes with modules

--- workshops/w3-node/index.js
+++ workshops/w3-node/index.js
@@ -1,8 +1,24 @@
-//add a reference to the module... 
-const testModuleVar = require('./testModule');
-//call the function name ... 
-console.log('Results: ' + testModuleVar.name());
-testModuleVar.setPetName("Santiago");
-console.log(testModuleVar.getPetName());
+// //add a reference to the module... 
+// const testModuleVar = require('./testModule');
+// //call the function name ... 
+// console.log('Results: ' + testModuleVar.name());
+// testModuleVar.setPetName("Santiago");
+// console.log(testModuleVar.getPetName());
 
-// testModuleVar.appendPetName();
+// // testModuleVar.appendPetName();
+
+
+//use the static ...
+const testModuleStat = require('./staticModule');
+console.log(testModuleStat);
+ 
+testModuleStat.passMessage("testMessage");
+testModuleStat.passMessage("testMessage_2");
+testModuleStat.passMessage("testMessage_3");
+ 
+// next var
+const testModuleStatTwo = require('./staticModule');
+console.log(testModuleStatTwo);
+testModuleStatTwo.passMessage("testMessage");
+testModuleStatTwo.passMessage("testMessage_2");
+testModuleStatTwo.passMessage("testMessage_3");
