2afa799bc290a265b627dcbfdb09a5df52840fa8
Sharon Ku <71299609+sharon-ku@users.noreply.github.com>
2022-09-20 20:21:06 -0400

    w3 add a date to a new file

--- workshops/WS_TEST_WEEK_3/writeI.js
+++ workshops/WS_TEST_WEEK_3/writeI.js
@@ -1,1 +1,5 @@
 const fs = require('fs');
+const fileContainer = './myFiles/fileA.txt';
+
+let data = "this is my test string written at "+(new Date)+"to "+fileContainer;
+fs.writeFileSync(fileContainer, data);
