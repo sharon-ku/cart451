316e86f4d90377db1b1816e3bf49f15274babf6a
Sharon Ku <71299609+sharon-ku@users.noreply.github.com>
2022-09-20 20:24:06 -0400

    w3 appending (adding) text to a file that already has text

--- workshops/WS_TEST_WEEK_3/writeI.js
+++ workshops/WS_TEST_WEEK_3/writeI.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const fileContainer = './myFiles/fileA.txt';
 
-let data = "this is my test string written at "+(new Date)+"to "+fileContainer;
-fs.writeFileSync(fileContainer, data);
+let dataThird = "this is my third test string written at "+(new Date)+"to "+fileContainer+"\n"+"more text on second line";
+fs.writeFileSync(fileContainer, dataThird);
+
+let textToAppend = "\n" + (new Date) + " Text copied and appended  to " + fileContainer;
+fs.appendFileSync(fileContainer, textToAppend);
