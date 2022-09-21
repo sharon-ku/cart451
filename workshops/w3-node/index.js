77c1732223b0ae3e6c7d3ce1e5f8b8abec711c97
Sharon Ku <71299609+sharon-ku@users.noreply.github.com>
2022-09-20 20:14:33 -0400

    w3: started a new example for i/o

--- workshops/w3-node/index.js
+++ workshops/w3-node/index.js
@@ -1,24 +1,19 @@
-// //add a reference to the module... 
-// const testModuleVar = require('./testModule');
-// //call the function name ... 
-// console.log('Results: ' + testModuleVar.name());
-// testModuleVar.setPetName("Santiago");
-// console.log(testModuleVar.getPetName());
-
-// // testModuleVar.appendPetName();
-
-
-//use the static ...
-const testModuleStat = require('./staticModule');
-console.log(testModuleStat);
+const testModuleClass = require('./classModule');
+//using "classes" -> no we are not having to "invoke" -> gives us a REF to the base definition....
+//The require statement gives you what many other languages call the base type.
+console.log(testModuleClass);
  
-testModuleStat.passMessage("testMessage");
-testModuleStat.passMessage("testMessage_2");
-testModuleStat.passMessage("testMessage_3");
+//make two seperate instances..
+let instA = new testModuleClass();
+console.log(instA);
+instA.passMessage("testMessage Again");
+instA.passMessage("testMessageTwo Again");
+instA.printMessages();
+console.log(instA);
+
  
-// next var
-const testModuleStatTwo = require('./staticModule');
-console.log(testModuleStatTwo);
-testModuleStatTwo.passMessage("testMessage");
-testModuleStatTwo.passMessage("testMessage_2");
-testModuleStatTwo.passMessage("testMessage_3");
+let instB = new testModuleClass();
+console.log(instB);
+instB.passMessage("testMessageOnB Again");
+instB.printMessages();
+console.log(instB);
