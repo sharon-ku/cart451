2a11c2aa6a54680c7363f7e1c620c7017c1dca78
Sharon Ku <71299609+sharon-ku@users.noreply.github.com>
2022-09-27 19:10:11 -0400

    w5: did example using callbacks

--- workshops/w5-promises/js/callbackOptionCART451.js
+++ workshops/w5-promises/js/callbackOptionCART451.js
@@ -16,99 +16,105 @@ const responseA = document.querySelector("#responseA");
 buttonCallA.addEventListener("click", () => {
   console.log("click");
 
+  // let returnedFruit = checkAuth_A(document.querySelector("#user-string-A").value);
+  // console.log(returnedFruit);
+  // changeString_A(returnedFruit);
+
+  checkAuth_C(document.querySelector("#user-string-A").value, changeString_C);
+
 });
 
-// /*** option 1 :: everything happens as single functions */
-// function checkAuth_A(userString) {
-//   //console.log(userString);
-//   setTimeout(() => {
-//     let userFruit = "";
-//     if (userString === "Sabine") {
-//       userFruit = "pineapple";
-//     } else {
-//       userFruit = "noFruit";
-//     }
-//     console.log("time-out one-a complete " + userFruit);
-//     return userFruit;
-//   }, 5000); // let 5 secs go past then send back
-// }
+/*** option 1 :: everything happens as single functions */
+function checkAuth_A(userString) {
+  //console.log(userString);
+  setTimeout(() => {
+    let userFruit = "";
+    if (userString === "Sabine") {
+      userFruit = "pineapple";
+    } else {
+      userFruit = "noFruit";
+    }
+    console.log("time-out one-a complete " + userFruit);
+    return userFruit;
+  }, 5000); // let 5 secs go past then send back
+}
 
-// function changeString_A(fruitString) {
-//   //console.log(userString);
-//   setTimeout(() => {
-//     let userFruitwithstars = fruitString.split("").join("*");
-//     console.log("time-out two-a complete " + fruitString);
-//     return userFruit;
-//   }, 5000); // let 5 secs go past then send back
-// }
+function changeString_A(fruitString) {
+  //console.log(userString);
+  setTimeout(() => {
+    let userFruitwithstars = fruitString.split("").join("*");
+    console.log("time-out two-a complete " + fruitString);
+    return userFruit;
+  }, 5000); // let 5 secs go past then send back
+}
 
-// /*** option 2 :: callback  */
-// //cb ==callback
-// function checkAuth_B(userString, cb) {
-//   //console.log(userString);
-//   setTimeout(() => {
-//     let userFruit = "";
-//     if (userString === "Sabine") {
-//       userFruit = "pineapple";
-//     } else {
-//       userFruit = "noFruit";
-//     }
-//     console.log("time-out one-b complete " + userFruit);
+/*** option 2 :: callback  */
+//cb ==callback
+function checkAuth_B(userString, cb) {
+  //console.log(userString);
+  setTimeout(() => {
+    let userFruit = "";
+    if (userString === "Sabine") {
+      userFruit = "pineapple";
+    } else {
+      userFruit = "noFruit";
+    }
+    console.log("time-out one-b complete " + userFruit);
 
-//     //CALL the callback function :)
-//     cb(userFruit);
-//   }, 5000); // let 5 secs go past then send back
-// }
+    //CALL the callback function :)
+    cb(userFruit);
+  }, 5000); // let 5 secs go past then send back
+}
 
-// function changeString_B(fruitString) {
-//   //console.log(userString);
-//   setTimeout(() => {
-//     let userFruitwithstars = fruitString.split("").join("*");
+function changeString_B(fruitString) {
+  //console.log(userString);
+  setTimeout(() => {
+    let userFruitwithstars = fruitString.split("").join("*");
 
-//     console.log("time-out two-b complete " + userFruitwithstars);
-//     return userFruitwithstars;
-//   }, 5000); // let 5 secs go past then send back
-// }
+    console.log("time-out two-b complete " + userFruitwithstars);
+    return userFruitwithstars;
+  }, 5000); // let 5 secs go past then send back
+}
 
 //for second-part of option callback - change string now also takes a callback parameter
 
-// function checkAuth_C(userString, cb) {
-//   //console.log(userString);
-//   setTimeout(() => {
-//     let userFruit = "";
-//     if (userString === "Sabine") {
-//       userFruit = "pineapple";
-//     } else {
-//       userFruit = "noFruit";
-//     }
-//     console.log("time-out one-c complete " + userFruit);
+function checkAuth_C(userString, cb) {
+  //console.log(userString);
+  setTimeout(() => {
+    let userFruit = "";
+    if (userString === "Sabine") {
+      userFruit = "pineapple";
+    } else {
+      userFruit = "noFruit";
+    }
+    console.log("time-out one-c complete " + userFruit);
 
-//     //CALL the callback function :)
-//     //AND the other callback
-//     cb(userFruit, sendBackToUser);
-//   }, 5000); // let 5 secs go past then send back
-// }
+    //CALL the callback function :)
+    //AND the other callback
+    cb(userFruit, sendBackToUser);
+  }, 5000); // let 5 secs go past then send back
+}
 
-// function changeString_C(fruitString, cb) {
-//   //console.log(userString);
-//   setTimeout(() => {
-//     let userFruitwithstars = fruitString.split("").join("*");
+function changeString_C(fruitString, cb) {
+  //console.log(userString);
+  setTimeout(() => {
+    let userFruitwithstars = fruitString.split("").join("*");
 
-//     console.log("time-out two-c complete " + userFruitwithstars);
-//     cb(userFruitwithstars);
+    console.log("time-out two-c complete " + userFruitwithstars);
+    cb(userFruitwithstars);
 
-//     //return userFruitwithstars;
-//   }, 5000); // let 5 secs go past then send back
-// }
+    //return userFruitwithstars;
+  }, 5000); // let 5 secs go past then send back
+}
 
-// function sendBackToUser(starredString) {
-//   //console.log(userString);
-//   setTimeout(() => {
-//     console.log("time-out three-c complete  and will now display to user");
-//     responseA.innerHTML = starredString;
+function sendBackToUser(starredString) {
+  //console.log(userString);
+  setTimeout(() => {
+    console.log("time-out three-c complete  and will now display to user");
+    responseA.innerHTML = starredString;
 
-//     //return userFruitwithstars;
-//   }, 5000); // let 5 secs go past then send back
-// }
+    //return userFruitwithstars;
+  }, 5000); // let 5 secs go past then send back
+}
 
 
