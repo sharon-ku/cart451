e829e77f56dc9d2ae83a2bacb0ddd43ea5f8fb6a
Sharon Ku <71299609+sharon-ku@users.noreply.github.com>
2022-09-27 19:58:34 -0400

    w5 used promises for same example

--- workshops/w5-promises/js/promiseOptionCART451.js
+++ workshops/w5-promises/js/promiseOptionCART451.js
@@ -10,7 +10,69 @@ const responseB = document.querySelector("#responseB");
 
 buttonCallB.addEventListener("click", function () {
   console.log("clicked B");
+  checkAuth_A(document.querySelector("#user-string-B").value)
+  .then(
+      // resolve
+      function (result) {
+      console.log("result"+result);
+      return changeString_A(result);
+    },
+    // reject
+    function (reject) {
+      console.log("reject"+reject);
+      return new Promise((resolve,reject) => {
+        reject("error occured");
+      })
+    }
+
+  )
+  .then(
+    function(result) {
+      console.log(result);
+    }
+  )
+  // what happens if I get reject anywhere before
+  .catch(
+    function(error) {
+      console.log("error occurred" + error)
+    }
+  )
 })
+
+
+
+/*** option 1 :: everything happens as single functions */
+function checkAuth_A(userString) {
+
+  return new Promise((resolve, reject)=>{
+    //console.log(userString);
+    setTimeout(() => {
+      let userFruit = "";
+      if (userString === "Sabine") {
+        userFruit = "pineapple";
+        resolve(userFruit);
+      } else {
+        userFruit = "noFruit";
+        reject(userFruit);
+      }
+      console.log("time-out one-a complete " + userFruit);
+      
+    }, 5000); // let 5 secs go past then send back
+  })
+  
+}
+
+function changeString_A(fruitString) {
+  return new Promise((resolve, reject)=> {
+    //console.log(userString);
+    setTimeout(() => {
+      let userFruitwithstars = fruitString.split("").join("*");
+      console.log("time-out two-a complete " + userFruitwithstars);
+      resolve(userFruitwithstars);
+    }, 5000); // let 5 secs go past then send back
+  })
+}
+
 
   
 //};
