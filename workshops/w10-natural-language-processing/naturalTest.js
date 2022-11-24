17da2be9211a29f6b9118e25edf5cf75dffeb0d8
sharon-ku <71299609+sharon-ku@users.noreply.github.com>
2022-11-24 13:05:56 -0500

    w10 stemming, sentence splitting, n-grams, parts of speech

--- workshops/w10-natural-language-processing/naturalTest.js
+++ workshops/w10-natural-language-processing/naturalTest.js
@@ -5,3 +5,21 @@ let tokenizer = new natural.WordTokenizer();
 let tokens = tokenizer.tokenize("the lazy dog jumped over the high mountains.");
 console.log(tokens);
 console.log(natural.PorterStemmer.stem(tokens[7]));
+let sentenceSplitter = new natural.SentenceTokenizer();
+
+// Ngrams
+let NGrams = natural.NGrams;
+
+let bigrams = NGrams.ngrams("the lazy dog jumped over the high mountains.", 5);
+console.log(bigrams);
+
+// Parts os speech
+const language = "EN";
+const defaultCategory = 'N';
+const defaultCategoryCapitalized = 'NNP';
+
+let lexicon = new natural.Lexicon(language, defaultCategory, defaultCategoryCapitalized);
+let ruleSet = new natural.RuleSet('EN');
+let tagger = new natural.BrillPOSTagger(lexicon, ruleSet);
+
+console.log(tagger.tag(tokens));
